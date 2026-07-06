afec507356b68063c32b33c22ae52c9bf7fed760
stravioofficial-web <stravio.official@gmail.com>
2026-07-06 12:46:28 +0530

    khushal 2

---
+++ src/components/CloudinaryImage.jsx
@@ -0,0 +1,28 @@
+'use client';
+
+import { useCloudinaryImage } from '../lib/cloudinaryAssets';
+
+export default function CloudinaryImage({ src, alt = '', ...props }) {
+  const resolvedSrc = useCloudinaryImage(src);
+
+  if (!src) return null;
+
+  return (
+    <img
+      src={resolvedSrc || src}
+      alt={alt}
+      referrerPolicy="no-referrer"
+
+
+      
+      {...props}
+    />
+  );
+
+
+
+
+
+
+
+}
